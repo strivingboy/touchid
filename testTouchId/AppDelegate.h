04876852c9df58594cc92335c5352d1fa50b6a03
qi hailong <hzqihailong@corp.netease.com>
2014-08-14 10:44:08 +0800

    Initial Commit

---
+++ testTouchId/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testTouchId
+//
+//  Created by hailong on 14-8-14.
+//  Copyright (c) 2014年 hailong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testTouchId/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testTouchId
+//
+//  Created by hailong on 14-8-14.
+//  Copyright (c) 2014年 hailong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
